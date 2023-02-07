a7605cefa63d52ed4579e77614cd960d61b58f1b
grepnick <124594745+grepnick@users.noreply.github.com>
2023-02-07 15:13:04 -0600

    Update Backup-Cisco-Config-via-SSH.py

--- Backup-Cisco-Config-via-SSH.py
+++ Backup-Cisco-Config-via-SSH.py
@@ -3,37 +3,40 @@
 
 import paramiko
 
-# Set the IP address of the Cisco device
-HOST = "192.168.1.1"
+# Set the list of IP addresses of the Cisco devices
+hosts = ["192.168.1.1", "192.168.1.2", "192.168.1.3"]
 
 # Set the username and password to use for the SSH connection
 user = "user"
 password = "password"
 
-# Set the filename to use for the configuration backup
-filename = "config_backup.txt"
+# Loop over the list of hosts
+for host in hosts:
+    # Set the filename to use for the configuration backup
+    filename = f"config_backup_{host}.txt"
 
-# Create an SSH client
-ssh = paramiko.SSHClient()
+    # Create an SSH client
+    ssh = paramiko.SSHClient()
 
-# Automatically add host keys for unknown hosts
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    # Automatically add host keys for unknown hosts
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-# Connect to the Cisco device
-ssh.connect(HOST, username=user, password=password)
+    # Connect to the Cisco device
+    ssh.connect(host, username=user, password=password)
 
-# Execute the "show run" command to retrieve the configuration
-stdin, stdout, stderr = ssh.exec_command("show run")
+    # Execute the "show run" command to retrieve the configuration
+    stdin, stdout, stderr = ssh.exec_command("show run")
 
-# Wait for the command to complete
-output = stdout.read().decode()
+    # Wait for the command to complete
+    output = stdout.read().decode()
 
-# Close the SSH connection
-ssh.close()
+    # Close the SSH connection
+    ssh.close()
 
-# Write the configuration to the specified file
-with open(filename, "w") as f:
-	f.write(output)
+    # Write the configuration to the specified file
+    with open(filename, "w") as f:
+        f.write(output)
 
-print(f"Configuration successfully backed up to {filename}")
+    print(f"Configuration successfully backed up for host {host} to {filename}")
+
 stdin.close()
